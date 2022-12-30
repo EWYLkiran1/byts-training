ad470a38dedc6ec8068ee9b9b2c317e5aa6f8c24
KIRANPRASATH <105300308+EWYLkiran1@users.noreply.github.com>
2022-12-29 21:26:34 -0800

    Time: 19 ms (38.85%), Space: 43.9 MB (71.66%) - LeetHub

---
+++ 0477-total-hamming-distance/0477-total-hamming-distance.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int totalHammingDistance(int[] nums) {
+        int ans = 0;
+        for(int i = 31;i >= 0;i--){
+            int zeros = 0;
+            int ones = 0;
+            for(int num : nums){
+                if((num&(1<<i)) > 0){
+                    ones++;
+                }
+                else{
+                    zeros++;
+                }
+            }
+
+            ans += (ones * zeros);
+        }
+
+        return ans;
+    }
+}
